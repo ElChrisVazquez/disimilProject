73d97cacc78d601f7b1282c3172712629809ef21
TheChrisVazquez <TheChrisVazquez@gmail.com>
2019-07-08 13:02:01 -0500

    Se creó el interfaz, colores y se añadieron imagenes

---
+++ src/colores/Colores.java
@@ -0,0 +1,17 @@
+package colores;
+
+import java.awt.Color;
+
+public class Colores {
+    
+    private Color colorFondo;
+
+    public Colores() {
+        colorFondo =  Color.decode("#646c74");
+    } 
+
+    public Color getColorFondo() {
+        return colorFondo;
+    }
+    
+}
--- src/interfaz/Interfaz.java
+++ src/interfaz/Interfaz.java
@@ -1,7 +1,43 @@
 package interfaz;
 
+import colores.Colores;// Importa clase de colores
+import java.awt.HeadlessException;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Interfaz extends JFrame{
     
+    private Colores colores;
+    private JMenuBar jmbBarra;
+    private JMenu jmArchivo;
+
+   public Interfaz() throws HeadlessException {
+       setSize(1440, 900);
+       setDefaultCloseOperation(3);
+       setLocationRelativeTo(null);
+       setLayout(null);
+       
+       //inicializa  clase colores
+       colores  = new Colores();
+       
+       //Pinta el fondo del frame
+       getContentPane().setBackground(colores.getColorFondo());
+       
+       jmbBarra  = new JMenuBar();
+       jmbBarra.setBounds(0, 0, 200, 35);
+       jmbBarra.setOpaque(false);
+       jmArchivo = new JMenu("Archivo");
+       jmbBarra.add(jmArchivo);
+       
+       
+       add(jmbBarra);
+       setVisible(true);
+    }
+    
+   
+    public static void main(String[] args) {
+        Interfaz interfaz = new Interfaz();
+    }
 }
